5d3ef9d8d653bb09a09053dcbe577adfcf968e02
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-01-02 16:40:09 +0100

    Test

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const run = async () => {
     if (body && body.match(re)) {
         notice("Replacing regex matched content in PR body");
         let a = body.replace(re, output);
-        let b = a.replace(/\n\n/img, '\n');
+        let b = a.replace('\n\n', '\n');
         console.log(body)
         console.log("----------------")
         console.log(a)
